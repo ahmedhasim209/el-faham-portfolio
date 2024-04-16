b58cb1e465b65fe4f4b621d49aed967195d3d467
Ahmmed Hashim <ahmedhasim209>
2024-04-17 00:07:59 +0200

    Added button in about us page

---
+++ js/about.js
@@ -0,0 +1,13 @@
+const buttonScroll = document.querySelectorAll(".time-scroll");
+
+function scrollSections(elements) {
+  elements.forEach((ele) => {
+    ele.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector(e.target.dataset.section).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+}
+scrollSections(buttonScroll);

4ce5e7cdcc123667d1c6a381d52c57f24c7314fe
Ahmmed Hashim <ahmedhasim209>
2024-07-26 23:22:44 +0300

    Create Best Seller section & add items to it

--- js/about.js
+++ js/about.js
@@ -1,13 +1,13 @@
-const buttonScroll = document.querySelectorAll(".time-scroll");
+// const buttonScroll = document.querySelectorAll(".time-scroll");
 
-function scrollSections(elements) {
-  elements.forEach((ele) => {
-    ele.addEventListener("click", (e) => {
-      e.preventDefault();
-      document.querySelector(e.target.dataset.section).scrollIntoView({
-        behavior: "smooth",
-      });
-    });
-  });
-}
-scrollSections(buttonScroll);
+// function scrollSections(elements) {
+//   elements.forEach((ele) => {
+//     ele.addEventListener("click", (e) => {
+//       e.preventDefault();
+//       document.querySelector(e.target.dataset.section).scrollIntoView({
+//         behavior: "smooth",
+//       });
+//     });
+//   });
+// }
+// scrollSections(buttonScroll);

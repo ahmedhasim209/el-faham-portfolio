9763e1f706a52e24bf3fe1670a04720c3b4a8e00
Ahmmed Hashim <ahmedhasim209>
2024-08-07 06:25:52 +0300

    Finish Responsive for mobile in home page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+links = document.getElementById("links");
+burgerIcon = document.getElementById("burger-icon");
+xIcon = document.getElementById("close");
+
+burgerIcon.addEventListener("click", () => {
+  links.classList.add("open");
+  links.classList.remove("close");
+});
+xIcon = document.getElementById("close");
+xIcon.addEventListener("click", () => {
+  links.classList.remove("open");
+  links.classList.add("close");
+});
 const numbers = [
   {
     number: 0,

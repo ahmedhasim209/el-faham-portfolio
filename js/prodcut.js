ebfce43bc6d731793ce2f3286e581515949dfcce
Ahmmed Hashim <ahmedhasim209>
2024-08-07 18:40:37 +0300

    Create & Finish responsive for mobile screens in products page

--- js/prodcut.js
+++ js/prodcut.js
@@ -1,3 +1,17 @@
+links = document.getElementById("links");
+burgerIcon = document.getElementById("burger-icon");
+xIcon = document.getElementById("close");
+
+burgerIcon.addEventListener("click", () => {
+  links.classList.add("open");
+  links.classList.remove("close");
+});
+xIcon = document.getElementById("close");
+xIcon.addEventListener("click", () => {
+  links.classList.remove("open");
+  links.classList.add("close");
+});
+
 const mainProducts = [
   {
     image: "../images/products-images/white-kidney-beans.webp",

3e6198d1623c33ca081d38a0f61d37679bb4da49
Ahmmed Hashim <ahmedhasim209>
2024-08-13 00:34:05 +0300

    Fix all proplems of side bar menu in all pages except conact us page

--- js/about.js
+++ js/about.js
@@ -11,3 +11,17 @@ xIcon.addEventListener("click", () => {
   links.classList.remove("open");
   links.classList.add("close");
 });
+links.onclick = (e) => {
+  e.stopPropagation();
+};
+
+//click anywhere out side of menu and burger icon to close a sidebar menu
+document.addEventListener("click", (e) => {
+  if (e.target !== burgerIcon && e.target !== links && e.target !== xIcon) {
+    if (links.classList.contains("open")) {
+      links.classList.remove("open");
+
+      links.classList.add("close");
+    }
+  }
+});

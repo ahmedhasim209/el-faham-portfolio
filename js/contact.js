c7cfdc5227b668fdaa35ab4b9f97414df17df79c
Ahmmed Hashim <ahmedhasim209>
2024-08-22 05:20:59 +0300

    Finish styling for Arabic & English versions on about us page

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,13 @@
 links = document.getElementById("links");
 burgerIcon = document.getElementById("burger-icon");
 xIcon = document.getElementById("close");
+console.log("ahmed");
 
 burgerIcon.addEventListener("click", () => {
   links.classList.add("open");
   links.classList.remove("close");
 });
-xIcon = document.getElementById("close");
+// xIcon = document.getElementById("close");
 xIcon.addEventListener("click", () => {
   links.classList.remove("open");
   links.classList.add("close");
